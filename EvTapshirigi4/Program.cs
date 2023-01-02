f29fd9f4920e9f1f3b0207bd824ff4421098e18d
kenanxasiyevv <kananakh@code.edu.az>
2023-01-03 02:32:47 +0400

    Add project files.

---
+++ EvTapshirigi4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EvTapshirigi4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Massivin uzunlugunu daxil edin: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"arr[{i}]= ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0 && arr[i] % 2 == 0)
+                {
+                    count++;
+                    sum += arr[i];
+                }
+            }
+            Console.WriteLine($"cem: {sum} say: {count}");
+
+        }
+    }
+}
